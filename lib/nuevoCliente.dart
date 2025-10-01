4c6230726d84b96955ec477d3b6e7c1c8e1b9867
LuisFernando5847 <fogl271000@gmail.com>
2025-10-01 13:26:51 -0600

    Modificacion de nuevos clientes

--- lib/nuevoCliente.dart
+++ lib/nuevoCliente.dart
@@ -1,15 +1,155 @@
 import 'package:chm_ios/main.dart';
+//import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
-class NuevoCliente extends StatelessWidget {
+class NuevoCliente extends StatefulWidget {
   const NuevoCliente({super.key});
+
+  @override
+  State<NuevoCliente> createState() => _NuevoClienteState();
+}
+
+class _NuevoClienteState extends State<NuevoCliente> {
+  // Variables para despues enviar por correo
+  final telefonoController = TextEditingController();
+  final notasController = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+    //Mostrar Snackbar despues de la pantalla cargue
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(
+          content: Text(
+            "Preciona el asistente para saber mas",
+            textAlign: TextAlign.center,
+            style: TextStyle(color: Colors.white),
+          ),
+          backgroundColor: Colors.blue,
+          duration: Duration(seconds: 6),
+        ),
+      );
+    });
+  }
+
+  void _mostrarDialogo() {
+    //Cierra el dialogo si esta abierto
+    ScaffoldMessenger.of(context).hideCurrentSnackBar();
+
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text("Gracias por elegirnos"),
+          content: SingleChildScrollView(
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Text("Ingresa un número telefónico, para contactarte."),
+                const SizedBox(height: 10),
+                //Campo de telefono
+                TextField(
+                  controller: telefonoController,
+                  keyboardType: TextInputType.number,
+                  maxLength: 10,
+                  inputFormatters: [
+                    FilteringTextInputFormatter.digitsOnly,
+                    LengthLimitingTextInputFormatter(10),
+                  ],
+                  decoration: const InputDecoration(
+                    labelText: "Número telefonico",
+                    counterText: "",
+                    border: OutlineInputBorder(),
+                  ),
+                ),
+                const SizedBox(height: 10),
+                Text("Anota una breve descripción de lo que necesitas y uno de nuestros asesores te atenderá."),
+                const SizedBox(height: 10),
+                //Campo Notas
+                TextField(
+                  controller: notasController,
+                  maxLines: 10,
+                  maxLength: 1000,
+                  decoration: const InputDecoration(
+                    labelText: "Notas",
+                    border: OutlineInputBorder(),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          actions: [
+            TextButton(
+              child: const Text("Cancelar"),
+              onPressed: () => Navigator.of(context).pop(),
+            ),
+            TextButton(
+              child: const Text("Enviar"),
+              onPressed: () {
+                String telefono = telefonoController.text.trim();
+                String notas = notasController.text.trim();
+
+                if (telefono.isEmpty || notas.isEmpty) {
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    const SnackBar(
+                      content: Text(
+                        'No dejes los campos vacios',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(color: Colors.white),
+                      ),
+                      duration: Duration(seconds: 3),
+                      backgroundColor: Colors.red,
+                    ),
+                  );
+                  return;
+                }
+
+                if (telefono.length < 10) {
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    const SnackBar(
+                      content: Text(
+                        'El número debe de tener 10 digitos',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(color: Colors.white),
+                      ),
+                      duration: Duration(seconds: 3),
+                      backgroundColor: Colors.deepOrangeAccent,
+                    ),
+                  );
+                  return;
+                }
+
+                //Aqui se procesan los datos
+
+                Navigator.of(context).pop();
+
+                ScaffoldMessenger.of(context).showSnackBar(
+                  SnackBar(
+                    content: Text(
+                      "Telefono $telefono \nNotas $notas",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(color: Colors.white),
+                    ),
+                    duration: Duration(seconds: 3),
+                    backgroundColor: Colors.green,
+                  ),
+                );
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text(
-          'CHM Móvil',
+          'CHM Movil',
           style: TextStyle(
             color: Colors.white,
             fontSize: 24,
@@ -33,7 +173,7 @@ class NuevoCliente extends StatelessWidget {
                         onPressed: () => Navigator.of(context).pop(),
                       ),
                       TextButton(
-                        child: const Text('Cerrar sesión'),
+                        child: const Text('Cerrar Sesión'),
                         onPressed: () {
                           Navigator.of(context).pop();
                           Navigator.pushAndRemoveUntil(
@@ -58,27 +198,29 @@ class NuevoCliente extends StatelessWidget {
           // Imagen de fondo
           Positioned.fill(
             child: Image.asset(
-              'assets/fondoAPP.png', // Cambia por tu imagen
-              fit: BoxFit.cover, // Para que ocupe toda la pantalla
+              'assets/Fondo_Azul.png',
+              fit: BoxFit.cover,
             ),
           ),
-
+          
           // Contenido central
           Center(
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                // Imagen central
-                Image.asset(
-                  'assets/animacion.gif', // Cambia por la imagen central que desees
-                  width: 150, // Ajusta tamaño
+                // Imagen central con gesture Detector
+                GestureDetector(
+                  onTap: _mostrarDialogo,
+                  child: Image.asset(
+                    'assets/animacion6.gif',
+                    width: 210,
+                  ),
                 ),
-
                 const SizedBox(height: 20),
-
+                
                 // Texto de bienvenida
-                Text(
-                  'Bienvenido',
+                const Text(
+                  'Bienveido',
                   style: TextStyle(
                     fontSize: 32,
                     fontWeight: FontWeight.bold,
